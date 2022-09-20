python/mypy

add basedmypy version info

mypy/main.py
@@ -26,7 +26,7 @@ from mypy.options import Options, BuildType
 from mypy.config_parser import get_config_module_names, parse_version, parse_config_file
 from mypy.split_namespace import SplitNamespace
 
-from mypy.version import __version__
+from mypy.version import __version__, __based_version__
 
 orig_stat: Final = os.stat
 MEM_PROFILE: Final = False  # If True, dump memory profile
@@ -295,6 +295,10 @@ HEADER: Final = """%(prog)s [-h] [-v] [-V] [more options; see below]
 
 
 DESCRIPTION: Final = """
+Basedmypy is a program that will type check your Python code.
+
+Based? Based on what?
+
 Mypy is a program that will type check your Python code.
 
 Pass in any files or folders you want to type check. Mypy will
@@ -488,7 +492,10 @@ def process_options(args: List[str],
     compilation_status = "no" if __file__.endswith(".py") else "yes"
     general_group.add_argument(
         '-V', '--version', action=CapturableVersionAction,
-        version='%(prog)s ' + __version__ + f" (compiled: {compilation_status})",
+        version=(
+            f'basedmypy {__based_version__} (compiled: {compilation_status})\n'
+            f'Based on %(prog)s {__version__}'
+        ),
         help="Show program's version number and exit",
         stdout=stdout)
 

setup.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 # alternative forms of installing, as suggested by README.md).
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
-from mypy.version import __version__ as version
+from mypy.version import __version__, __based_version__, based_version_info
 
 description = 'Optional static typing for Python'
 long_description = '''
@@ -80,7 +80,10 @@ class CustomPythonBuild(build_py):
         path = os.path.join(self.build_lib, 'mypy')
         self.mkpath(path)
         with open(os.path.join(path, 'version.py'), 'w') as stream:
-            stream.write('__version__ = "{}"\n'.format(version))
+            stream.write('from mypy.versionutil import VersionInfo\n')
+            stream.write(f'__version__ = "{__version__}"\n')
+            stream.write(f'based_version_info = {based_version_info!r}\n')
+            stream.write(f'__based_version__ = "{__based_version__}"\n')
 
     def run(self):
         self.execute(self.pin_version, ())
@@ -197,7 +200,7 @@ classifiers = [
 ]
 
 setup(name='mypy',
-      version=version,
+      version=__based_version__,
       description=description,
       long_description=long_description,
       author='Jukka Lehtosalo',

Zeckie/baselinedmypy

mypy/main.py
@@ -21,7 +21,7 @@ from mypy.fscache import FileSystemCache
 from mypy.modulefinder import BuildSource, FindModuleCache, SearchPaths, get_search_dirs, mypy_path
 from mypy.options import BuildType, Options
 from mypy.split_namespace import SplitNamespace
-from mypy.version import __version__
+from mypy.version import __based_version__, __version__
 
 orig_stat: Final = os.stat
 MEM_PROFILE: Final = False  # If True, dump memory profile
@@ -308,6 +308,10 @@ HEADER: Final = """%(prog)s [-h] [-v] [-V] [more options; see below]
 
 
 DESCRIPTION: Final = """
+Basedmypy is a program that will type check your Python code.
+
+Based? Based on what?
+
 Mypy is a program that will type check your Python code.
 
 Pass in any files or folders you want to type check. Mypy will
@@ -508,7 +512,10 @@ def process_options(
         "-V",
         "--version",
         action=CapturableVersionAction,
-        version="%(prog)s " + __version__ + f" (compiled: {compilation_status})",
+        version=(
+            f"basedmypy {__based_version__} (compiled: {compilation_status})\n"
+            f"Based on %(prog)s {__version__}"
+        ),
         help="Show program's version number and exit",
         stdout=stdout,
     )

setup.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
 
-from mypy.version import __version__ as version
+from mypy.version import __based_version__, __version__, based_version_info
 
 description = "Optional static typing for Python"
 long_description = """
@@ -83,7 +83,10 @@ class CustomPythonBuild(build_py):
         path = os.path.join(self.build_lib, "mypy")
         self.mkpath(path)
         with open(os.path.join(path, "version.py"), "w") as stream:
-            stream.write(f'__version__ = "{version}"\n')
+            stream.write("from mypy.versionutil import VersionInfo\n")
+            stream.write(f'__version__ = "{__version__}"\n')
+            stream.write(f"based_version_info = {based_version_info!r}\n")
+            stream.write(f'__based_version__ = "{__based_version__}"\n')
 
     def run(self):
         self.execute(self.pin_version, ())
@@ -208,7 +211,7 @@ classifiers = [
 
 setup(
     name="mypy",
-    version=version,
+    version=__based_version__,
     description=description,
     long_description=long_description,
     author="Jukka Lehtosalo",